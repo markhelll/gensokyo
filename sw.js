4aba160106e6993612fc31001012102e5db670e1
markhelll <tomohiro.seki@gmail.com>
2026-06-28 17:02:34 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+// 幻想郷塾 Service Worker - シンプルなオフラインキャッシュ
+const CACHE_NAME = 'gensokyo-school-v1';
+const URLS_TO_CACHE = ['./', './index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE).catch(() => {
+        // ファイル名が違う場合(例: 幻想郷塾.html)でも失敗しないようにする
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          if (response && response.status === 200) {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return response;
+        })
+        .catch(() => cached);
+    })
+  );
+});
